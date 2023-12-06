49db0be8b64d848b54c2a8fa0cc92aabbc6a0851
Aashay-Bharadwaj <98194521+Aashay-Bharadwaj@users.noreply.github.com>
2023-12-05 19:39:14 -0800

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -9,11 +9,9 @@ def call() {
         stages {
             stage('Build') {
                 steps {
-                    dir('../') {
                         // Install dependencies from requirements.txt
                         sh 'pip install -r requirements.txt --break-system-packages'
                         sh 'pip install --upgrade flask --break-system-packages'
-                    }
                 }
             }
             
